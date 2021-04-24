9833849a2348151e900a07ec82a470f3b9170296
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-24 06:42:34 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,14 @@
 class Translation(object):
-    START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
-
+    START_TEXT = """Haii 😊
+Jika Kamu Ingin Membuat App ID sama Api HASH Pada Akun Telegram Kamu,
+Silahkan ketik Nomor HP Anda disini, Wajib (+62) ya 😁
 /start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
-
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
-
+    AFTER_RECVD_CODE_TEXT = """✔️ Berhasil...
+Silahkan Cek Pesan Masuk di Telegram Kamu, Ambil Kode Yang Telah Dikirim Oleh Pihak Telegram!
+Jika Sudah Di Copy, Silahkan Kirim Kode Kamu Disini.
 /start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
-    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    BEFORE_SUCC_LOGIN = "Mendapatkan Kode. Scarpping web page ..."
+    ERRED_PAGE = "Terjadi error. Gagal untuk mendapatkan app id. \n\nSilahkan Hubungi @SyndicateTwenty4"
+    CANCELLED_MESG = "Goodbye! Mohon re /start Bot Untuk Memulai Ulang Kembali."
+    IN_VALID_CODE_PVDED = "Mohon Maaf, Kode Dari Telegram Yang Kamu Masukkan Tidak Valid.\n Mohon Periksa Kembali, Terimakasih."
+    IN_VALID_PHNO_PVDED = "Mohon Maaf, Masukkan Nomor Telepon Kamu Dengan Awalan Kode Negara, Seperti +62857"
