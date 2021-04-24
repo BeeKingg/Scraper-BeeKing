dc83a1d2313eab611a5c2bfdc7648a2502f86bc3
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-24 07:03:18 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,11 +2,13 @@ class Translation(object):
     START_TEXT = """Haii 😊
 Jika Kamu Ingin Membuat App ID sama Api HASH Pada Akun Telegram Kamu,
 Silahkan ketik Nomor HP Anda disini, Wajib (+62) ya 😁
-/start at any stage to re-enter your details"""
+📌**Noted :** Ketik atau Pencet /start Pada Tahap Manapun Untuk Memasukkan Kembali Detail Anda Jika Terjadi Kesalahan.
+"""
     AFTER_RECVD_CODE_TEXT = """✔️ Berhasil...
 Silahkan Cek Pesan Masuk di Telegram Kamu, Ambil Kode Yang Telah Dikirim Oleh Pihak Telegram!
 Jika Sudah Di Copy, Silahkan Kirim Kode Kamu Disini.
-/start at any stage to re-enter your details"""
+📌**Noted :** Ketik atau Pencet /start Pada Tahap Manapun Untuk Memasukkan Kembali Detail Anda Jika Terjadi Kesalahan.
+"""
     BEFORE_SUCC_LOGIN = "Mendapatkan Kode. Scarpping web page ..."
     ERRED_PAGE = "Terjadi error. Gagal untuk mendapatkan app id. \n\nSilahkan Hubungi @SyndicateTwenty4"
     CANCELLED_MESG = "Goodbye! Mohon re /start Bot Untuk Memulai Ulang Kembali."
