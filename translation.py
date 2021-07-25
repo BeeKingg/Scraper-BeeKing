f23af9f0eed39a3a9a93554aa87efab332140835
BeeKingg <87557652+BeeKingg@users.noreply.github.com>
2021-07-25 23:28:28 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -17,7 +17,7 @@ Jika Sudah Di Copy, Silahkan Kirim Kode Kamu Disini.
 📑 Noted : Ketik atau Tekan /start Pada Tahap Manapun Untuk Memasukkan Kembali Registrasi Anda Jika Terjadi Kesalahan.
 """
     BEFORE_SUCC_LOGIN = "Mendapatkan Kode. Scarpping web page ..."
-    ERRED_PAGE = "Terjadi error. Gagal untuk mendapatkan app id. \n\nSilahkan Hubungi @SyndicateTwenty4"
+    ERRED_PAGE = "Terjadi error. Gagal untuk mendapatkan app id. \n\nSilahkan Hubungi @boyfriendnice"
     CANCELLED_MESG = "Goodbye! Mohon re /start Bot Untuk Memulai Ulang Kembali."
     IN_VALID_CODE_PVDED = "Mohon Maaf, Kode Dari Telegram Yang Kamu Masukkan Tidak Valid.\n Mohon Periksa Kembali, Terimakasih."
     IN_VALID_PHNO_PVDED = "Mohon Maaf, Masukkan Nomor Telepon Kamu Dengan Awalan Kode Negara, Seperti +62857"
